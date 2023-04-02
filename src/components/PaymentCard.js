d6e73800bd505f10bd918a5de1f0777aa3b6fb7d
siddiqbarbhuiya <siddiqbarbhuiya@outlook.com>
2023-04-02 13:23:53 +0530

    upadted

--- src/components/PaymentCard.js
+++ src/components/PaymentCard.js
@@ -1,15 +1,39 @@
-import React from "react";
-import {FiChevronRight} from 'react-icons/fi'
+import React, { useState } from "react";
+import { FiChevronRight } from "react-icons/fi";
 import "./paymentCard.css";
 const PaymentCard = () => {
+  const [number, setNumber] = useState("");
+
+  const saveNumber = () => {
+    localStorage.setItem("mobile", number);
+  };
   return (
     <form className="payment__card">
       <div className="new-expense__controle">
-        <input type="tel" placeholder="Enter your mobile number" required />
-        
+        <input
+          type="text"
+          pattern="\d*"
+          maxLength={10}
+          placeholder="Enter your mobile number"
+          required
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
       </div>
       <div className="new-expense__action">
-      <button type="submit">Wow ! Get My Payment Card <span className="icons__container"><FiChevronRight className="icons" /></span> </button>
+        <button
+          type="submit"
+          style={
+            number.length === 10
+              ? { backgroundColor: "#00baf2", borderColor: "#ccc" }
+              : null
+          }
+          onClick={saveNumber}>
+          Wow ! Get My Payment Card{" "}
+          <span className="icons__container">
+            <FiChevronRight className="icons" />
+          </span>{" "}
+        </button>
       </div>
     </form>
   );
